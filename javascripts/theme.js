251deb475a8460c03a445af5084b8cbe3c118551
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2012-11-01 20:59:30 -0400

    Multi-value field fix applied on form update
    - moved analytics code to the top
    - created a redmine object to create methods on
    - multiple value custom fields are fixed if the form is updated (for
    example, by changing the status)
    - fixed the path to fontawesome in the css

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,41 +1,3 @@
-(function() {
-	"use strict";
-
-	$(document).ready(function() {
-
-		// I don't like how multiple value fields look, but can't uncheck that box.
-		function fixMultipleValueCustomField(id) {
-			var selected = false;
-			var $select = $("#" + id);
-
-			// Check to see if one of the options has been selected.
-			$select.children().each(function() {
-				if ($(this).attr("selected") === "selected") {
-					selected = true;
-				}
-			});
-
-			// Turn of multiple selection, and add an empty option.
-			$select.removeAttr("multiple").prepend("<option value=\"\"></option>");
-
-			// If no options have been selected, then the empty option will be.
-			if (!selected) {
-				$select.val("");
-			}
-		}
-
-		// Operating system.
-		fixMultipleValueCustomField("issue_custom_field_values_1");
-		
-		// Browser.
-		fixMultipleValueCustomField("issue_custom_field_values_7");
-
-		// Mobile device.
-		fixMultipleValueCustomField("issue_custom_field_values_12");
-	});
-}());
-
-
 // Google Analytics
 /*jshint eqeqeq: false*/
 /*jshint strict: false*/
@@ -47,3 +9,67 @@ _gaq.push(['_trackPageview']);
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+(function() {
+	"use strict";
+
+	var redmine = {};
+
+	redmine.init = function() {
+
+		// I don't like how multiple value fields look, but can't uncheck that box.
+		// TODO: If you're updating the issue, and change the project, tracker,
+		// or status, the custom fields reload with the original display. I could
+		// also just modify the database to fix the issue...
+		this.fixMultipleValueCustomFields();
+
+		// Override
+		// Fix multiple value custom fields if the issue form is updated.
+		window.updateIssueFrom = function(url) {
+			$.ajax({
+				url: url,
+				type: 'post',
+				data: $('#issue-form').serialize(),
+				success: function() {
+					redmine.fixMultipleValueCustomFields();
+				}
+			});
+		};
+	};
+
+	redmine.fixMultipleValueCustomFields = function() {
+
+		// Operating system.
+		this.fixMultipleValueCustomField("issue_custom_field_values_1");
+
+		// Browser.
+		this.fixMultipleValueCustomField("issue_custom_field_values_7");
+
+		// Mobile device.
+		this.fixMultipleValueCustomField("issue_custom_field_values_12");
+	};
+
+	redmine.fixMultipleValueCustomField = function(id) {
+		var selected = false;
+		var $select = $("#" + id);
+
+		// Check to see if one of the options has been selected.
+		$select.children().each(function() {
+			if ($(this).attr("selected") === "selected") {
+				selected = true;
+			}
+		});
+
+		// Turn of multiple selection, and add an empty option.
+		$select.removeAttr("multiple").prepend("<option value=\"\"></option>");
+
+		// If no options have been selected, then the empty option will be.
+		if (!selected) {
+			$select.val("");
+		}
+	};
+
+	$(document).ready(function() {
+		redmine.init();
+	});
+}());
